697213f0087267b59f4292d55f0a682a2f01eeae
DiYifei <diyifei@gmail.com>
2018-07-08 14:21:14 +0800

    export

--- src/main/resources/static/report/inventoryReport.js
+++ src/main/resources/static/report/inventoryReport.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var app = angular.module('inventory', ['ngAnimate', 'ngSanitize', 'ng-bs3-datepicker', 'ui.bootstrap', 'ui.grid']);
+var app = angular.module('inventory', ['ngAnimate', 'ngSanitize', 'ng-bs3-datepicker', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.exporter', 'ui.grid.selection', 'ui.grid.moveColumns']);
 
 app.filter('calculatePL', function () {
-  return function (input, cost, income, scope) {
-    return (input[income] - input[cost] * scope.fxRate).toFixed(2);
+  return function (input, cost, income, fxRate) {
+    return (input[income] - input[cost] * fxRate).toFixed(2);
   };
 });
 
@@ -16,26 +16,59 @@ app.controller('reportCtrl',
     $scope.page = "reportInventory";
     $scope.columns = [
         { field: 'name'},
-        { field: 'sellQty', displayName: 'Qty'},
-        { field: 'unitIncome', displayName: 'u.i ($)'},
-        { field: 'totalIncome', displayName: 't.i ($)'},
-        { field: 'unitCost', displayName: 'u.c (¥)'},
-        { field: 'totalCost', displayName: 't.c (¥)'},
-        { field: uiGridConstants.ENTITY_BINDING, displayName:'u.P/L ($)', cellFilter: 'calculatePL: "unitCost": "unitIncome": grid.appScope'},
-        { field: uiGridConstants.ENTITY_BINDING, displayName:'t.P/L ($)', cellFilter: 'calculatePL: "totalCost": "totalIncome": grid.appScope'}
-
+        { field: 'sellQty', displayName: 'Sell Qty'},
+        { field: 'unitIncome', displayName: 'Unit Income ($)'},
+        { field: 'totalIncome', displayName: 'Total Income ($)'},
+        { field: 'unitCost', displayName: 'Unit Cost (¥)'},
+        { field: 'totalCost', displayName: 'Total Cost (¥)'},
+        { field: uiGridConstants.ENTITY_BINDING, displayName:'Unit P/L ($)', cellFilter: 'calculatePL: "unitCost": "unitIncome": grid.appScope.fxRate'},
+        { field: uiGridConstants.ENTITY_BINDING, displayName:'Total P/L ($)', cellFilter: 'calculatePL: "totalCost": "totalIncome": grid.appScope.fxRate'}
     ];
 
     $scope.gridOptions = {
+        exporterMenuCsv: false,
         enableSorting: true,
+        enableColumnResizing: true,
+        exporterMenuCsv: false,
+        exporterPdfHeader: function () {
+            return { text: $scope.from + "~" + $scope.to, style: 'headerStyle' };
+        },
+        exporterMenuSelectedData: false,
+        exporterMenuVisibleData: false,
+        exporterColumnScaleFactor: 9,
+        appScopeProvider: $scope,
+        enableGridMenu: true,
         columnDefs: $scope.columns,
+        exporterHeaderFilterUseName: true,
         onRegisterApi: function(gridApi) {
             $scope.gridApi = gridApi;
+        },
+        exporterFieldCallback: function (grid, row, col, value) {
+          return grid.getCellDisplayValue(row, col);
+        },
+        exporterExcelCustomFormatters: function ( grid, workbook, docDefinition ) {
+            var formatters = {};
+            var stylesheet = workbook.getStyleSheet();
+            var boldStyle = stylesheet.createFontStyle({
+                size: 12, fontName: 'Calibri', bold: true
+            });
+            formatters['bold'] = stylesheet.createFormat({"font": boldStyle.id});
+            Object.assign(docDefinition.styles , formatters);
+            return docDefinition;
+        },
+        exporterExcelHeader: function (grid, workbook, sheet, docDefinition) {
+            sheet.data.push([{ value: $scope.from + ' ~ ' + $scope.to , metadata: {style: docDefinition.styles['bold'].id}}]);
         }
     };
 
     $scope.$watch('entities', function() {
         $scope.gridOptions.data = $scope.entities;
+    });
+
+    $scope.$watchGroup(['from', 'to'], function() {
+        $scope.gridOptions.exporterExcelFilename = 'InventoryReport(' + $scope.from + '~' + $scope.to + ').xlsx';
+        $scope.gridOptions.exporterPdfFilename = 'InventoryReport(' + $scope.from + '~' + $scope.to + ').pdf';
+        $scope.gridOptions.exporterExcelSheetName = $scope.from + '~' + $scope.to;
     });
 
     $scope.search = function (from, to) {
